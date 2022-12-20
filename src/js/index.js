c06984a5102ced59a917cbeddd4d8a0d0457e74d
SpicoDer <johncarlo.sarmiento.b@bulsu.edu.ph>
2022-12-20 18:18:50 +0800

    create map section.

--- src/js/index.js
+++ src/js/index.js
@@ -3,16 +3,34 @@ const navSign = document.querySelector('.nav-sign');
 const menu = document.querySelector('.icon-menu');
 const closeIcon = document.querySelector('.icon-close');
 
-menu.addEventListener('click', function () {
-  navOthers.classList.toggle('open');
-  navSign.classList.toggle('open');
-  menu.classList.toggle('open');
-  closeIcon.classList.toggle('open');
-});
+// menu.addEventListener('click', function () {
+//   navOthers.classList.toggle('open');
+//   navSign.classList.toggle('open');
+//   menu.classList.toggle('open');
+//   closeIcon.classList.toggle('open');
+// });
 
-closeIcon.addEventListener('click', function () {
-  navOthers.classList.toggle('open');
-  navSign.classList.toggle('open');
-  menu.classList.toggle('open');
-  closeIcon.classList.toggle('open');
-});
+// closeIcon.addEventListener('click', function () {
+//   navOthers.classList.toggle('open');
+//   navSign.classList.toggle('open');
+//   menu.classList.toggle('open');
+//   closeIcon.classList.toggle('open');
+// });
+
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: 14.9546886, lng: 120.910974 };
+  // The map, centered at Uluru
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 15,
+    center: uluru,
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+}
+
+window.initMap = initMap;
