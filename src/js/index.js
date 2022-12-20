eeda82877426e5799e774619ea90258915b28c59
SpicoDer <johncarlo.sarmiento.b@bulsu.edu.ph>
2022-12-20 21:52:27 +0800

    Refactor other components.

--- src/js/index.js
+++ src/js/index.js
@@ -2,20 +2,23 @@ const navOthers = document.querySelector('.nav-others');
 const navSign = document.querySelector('.nav-sign');
 const menu = document.querySelector('.icon-menu');
 const closeIcon = document.querySelector('.icon-close');
+const navLinks = document.querySelector('.nav-links');
 
-// menu.addEventListener('click', function () {
-//   navOthers.classList.toggle('open');
-//   navSign.classList.toggle('open');
-//   menu.classList.toggle('open');
-//   closeIcon.classList.toggle('open');
-// });
+menu.addEventListener('click', function () {
+  navOthers.classList.toggle('open');
+  navSign.classList.toggle('open');
+  menu.classList.toggle('open');
+  navLinks.classList.toggle('open');
+  closeIcon.classList.toggle('open');
+});
 
-// closeIcon.addEventListener('click', function () {
-//   navOthers.classList.toggle('open');
-//   navSign.classList.toggle('open');
-//   menu.classList.toggle('open');
-//   closeIcon.classList.toggle('open');
-// });
+closeIcon.addEventListener('click', function () {
+  navLinks.classList.toggle('open');
+  navOthers.classList.toggle('open');
+  navSign.classList.toggle('open');
+  menu.classList.toggle('open');
+  closeIcon.classList.toggle('open');
+});
 
 // Initialize and add the map
 function initMap() {
